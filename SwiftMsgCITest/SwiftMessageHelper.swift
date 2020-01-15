6f8bb94b87a94572fe3e9653377dbf05d19340c8
WimRigil <tanudjaja@mac.com>
2020-01-15 13:24:51 -0500

    First Committed

---
+++ SwiftMsgCITest/SwiftMessageHelper.swift
@@ -0,0 +1,14 @@
+//
+//  SwiftMessageHelper.swift
+//  SwiftMsgCITest
+//
+//  Created by Wim Tanudjaja on 1/15/20.
+//  Copyright © 2020 Wim Tanudjaja. All rights reserved.
+//
+
+import UIKit
+import SwiftMessages
+
+class SwiftMessageHelper: NSObject {
+
+}
--- SwiftMsgCITest/ViewController.swift
+++ SwiftMsgCITest/ViewController.swift
@@ -14,7 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func buttonPressed(sender: UIButton) {
+        
+        
+        
+    }
 
 }
 
