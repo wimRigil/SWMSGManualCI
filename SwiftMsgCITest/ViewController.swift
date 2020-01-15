6a8bab3ad6d9cb89f091fd7876ea44facfe64acc
WimRigil <tanudjaja@mac.com>
2020-01-15 13:03:46 -0500

    Initial Commit

---
+++ SwiftMsgCITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftMsgCITest
+//
+//  Created by Wim Tanudjaja on 1/15/20.
+//  Copyright © 2020 Wim Tanudjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
