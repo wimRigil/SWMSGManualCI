513caee2e9e6e6fec030500b6d1fc5e5eaf5081b
WimRigil <tanudjaja@mac.com>
2020-01-16 10:23:56 -0500

    adding Alamofire

---
+++ SwiftMsgCITest/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  SwiftMsgCITest
+//
+//  Created by Wim Tanudjaja on 1/16/20.
+//  Copyright © 2020 Wim Tanudjaja. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class DataManager: NSObject {
+
+}
